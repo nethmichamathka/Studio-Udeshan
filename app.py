626870344d2cb381140886403fb4701376844c71
nethmichamathka <chamathkanethmi11@gmail.com>
2024-10-09 13:44:56 +0530

    serv

--- app.py
+++ app.py
@@ -2,9 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Home Page Route
 @app.route('/')
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
+# Services Page Route
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+# About Page Route
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Contact Page Route
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
     app.run(debug=True)
